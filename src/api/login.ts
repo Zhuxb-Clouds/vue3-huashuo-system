67e8507440e92d4303ed26e866dac6b7a2e07021
Zhuxb <ZhuxbClouds@gmail.com>
2022-07-02 19:10:36 +0800

    feat：user接口增加api

--- src/api/login.ts
+++ src/api/login.ts
@@ -2,7 +2,7 @@ import req from '../libs/axios';
 export default {
     Login(data: any) {
         return req({
-            url: `/user/login`,
+            url: `api/user/login`,
             headers: { "Content-Type": "application/json" },
             method: 'post',
             data: data
