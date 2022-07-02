202ea3da6a97b150d1e694d57064afe6c689b3e0
Zhuxb <ZhuxbClouds@gmail.com>
2022-07-02 12:46:08 +0800

    fix:自动高度文本域

--- src/components/CardPopup.vue
+++ src/components/CardPopup.vue
@@ -12,7 +12,7 @@
                 </el-select>
             </el-form-item>
             <el-form-item label="卡牌名" prop="front">
-                <el-input v-model="form.front" />
+                <el-input v-model="form.front" type="textarea" autosize />
             </el-form-item>
             <el-form-item label="卡牌描述" prop="back">
                 <el-input v-model="form.back" type="textarea" autosize />
